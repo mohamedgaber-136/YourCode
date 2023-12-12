f9c6ba379f3f6ef8c2d13165af5802497cb64cb8
zolfa <unknown.zolfa@gmail.com>
2023-12-12 03:51:00 +0200

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import NavbarComp from "./Components/NavBar/Navbar"
+import NavbarComp from "./Components/NavBar/Navbar";
+import OurCoursesSection from "./Components/OurCourses/OurCoursesSection";
+import { FooterBar } from "./Components/FooterBar/FooterBar";
 
 function App() {
   return (
     <>
-    <NavbarComp/>
+      <NavbarComp />
+      <OurCoursesSection />
+      <FooterBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/FooterCol/FooterCol.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import logo from "../../assets/Logo.png";
+import { SiGmail } from "react-icons/si";
+import { BsTelephoneFill } from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
+
+const FooterCol = ({ title, list }) => {
+  const icons = [
+    <SiGmail color="grey" />,
+    <BsTelephoneFill color="grey" />,
+    <FaLocationDot color="grey" />,
+  ];
+
+  return (
+    <Col lg="3" md="6" className="mb-4">
+      {/* logo */}
+      {title == "logo" ? (
+        <img className="pt-2 pb-2" src={logo} />
+      ) : (
+        <h5 className="text-uppercase">{title}</h5>
+      )}
+
+      {title == "Social Profiles" ? (
+        <ul className="list-unstyled d-inline-flex gap-2">
+          {list.map((ele) => (
+            <li className="bg-light px-2 py-1 rounded-1">
+              <a href="#!" className="text-dark text-decoration-none">
+                {ele}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul className="list-unstyled ">
+          {list.map((ele, index) => (
+            <li>
+              {title == "logo" && <span className="pe-2">{icons[index]}</span>}
+              {/* <span>{icons[index]}</span> */}
+              <a href="#!" className="text-dark text-decoration-none">
+                {ele}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Col>
+  );
+};
+
+export default FooterCol;
