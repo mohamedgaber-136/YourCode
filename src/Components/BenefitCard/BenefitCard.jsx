9f245d5ed6e5dd7ab6941cffaa16af0e7aae4b1b
mohamedgaber-136 <mohamedd.gaberr.13696@gmail.com>
2023-12-13 16:07:59 +0200

    w

--- src/Components/BenefitCard/BenefitCard.jsx
+++ src/Components/BenefitCard/BenefitCard.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import styles from "./BenefitCard.module.css";
 import { IoArrowRedoOutline } from "react-icons/io5";
-import Aos from 'aos'
 export const BenefitCard = ({ item, index }) => {
-  useEffect(()=>{
-    Aos.init()
-  },[])
+
   return (
-    <Col lg="4" md="6" className="p-2">
+    
+    <Col lg="4" md="6" className="p-2 s">
       <div
         className={`${styles.card} bg-body p-4 rounded-2 gap-3 d-flex flex-column justify-content-between align-items-start shadow-sm `}
       >
